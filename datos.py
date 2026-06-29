798de04366e75e890dd8e06fe5e9c2ea392cb9fa
Marcos <romeromarcos320@gmail.com>
2026-06-29 08:52:30 -0300

    Codigo terminado de los modulos datos.py y autenticacion.py

---
+++ datos.py
@@ -0,0 +1,11 @@
+# Datos del usuario de prueba (usuario, contraseña, saldo inicial)
+
+LIM_EXTRACCION = 200000
+MAX_INTENTOS = 3
+
+cuentas_prueba = {
+    "marcos" : {"contraseña": "1234", "saldo": 1000.0, "bloqueado": False},
+    "nicolas" : {"contraseña": "3456", "saldo": 500.0, "bloqueado": False},
+    "nahuel" : {"contraseña": "4312", "saldo": 10000.0, "bloqueado": False},
+    "juan" : {"contraseña": "6767", "saldo": -200.0, "bloqueado": False},
+}
